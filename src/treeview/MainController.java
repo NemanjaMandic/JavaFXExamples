98f0dfb92131b72ae7a7c1883ad5949be05f1bd3
NemanjaMandic <nemanjans84@gmail.com>
2016-01-28 19:03:46 +0100

    TreeView and listeners

--- src/treeview/MainController.java
+++ src/treeview/MainController.java
@@ -13,6 +13,9 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
 
 public class MainController implements Initializable {
@@ -20,19 +23,25 @@ public class MainController implements Initializable {
   @FXML
   public TreeView<String> treeview;
     
+  Image icon = new Image(getClass().getResourceAsStream("/img/folder.png"));
+   
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-     
+    private void mouseClick(MouseEvent event) {
+        if(event.getClickCount() == 2){
+          TreeItem<String> item = treeview.getSelectionModel().getSelectedItem();
+          System.out.println(item.getValue());
+        }
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        TreeItem<String> root = new TreeItem<>("Root");
-      
+        TreeItem<String> root = new TreeItem<>("Root", new ImageView(icon));
+        root.setExpanded(true);
        
-        TreeItem<String> nodeA = new TreeItem<>("Node A");
-        TreeItem<String> nodeB = new TreeItem<>("Node B");
-        TreeItem<String> nodeC = new TreeItem<>("Node C");
+        TreeItem<String> nodeA = new TreeItem<>("Node A", new ImageView(icon));
+        TreeItem<String> nodeB = new TreeItem<>("Node B", new ImageView(icon));
+        TreeItem<String> nodeC = new TreeItem<>("Node C", new ImageView(icon));
+        nodeA.setExpanded(true);
         
        /*
         root.getChildren().add(nodeA);
@@ -42,9 +51,9 @@ public class MainController implements Initializable {
         
         root.getChildren().addAll(nodeA, nodeB, nodeC);
         
-         TreeItem<String> nodeA1 = new TreeItem<>("Node A1");
-         TreeItem<String> nodeB1 = new TreeItem<>("Node B1");
-         TreeItem<String> nodeC1 = new TreeItem<>("Node C1");
+         TreeItem<String> nodeA1 = new TreeItem<>("Node A1", new ImageView(icon));
+         TreeItem<String> nodeB1 = new TreeItem<>("Node B1", new ImageView(icon));
+         TreeItem<String> nodeC1 = new TreeItem<>("Node C1", new ImageView(icon));
          
         nodeA.getChildren().addAll(nodeA1, nodeB1, nodeC1);
         
