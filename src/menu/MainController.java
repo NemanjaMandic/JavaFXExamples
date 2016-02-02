c73b720351bbcebd9dcd0b8a8c1996af034a27f0
NemanjaMandic <nemanjans84@gmail.com>
2016-02-02 06:08:45 +0100

    added checkboxes

--- src/menu/MainController.java
+++ src/menu/MainController.java
@@ -9,7 +9,10 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
 
 /**
  * FXML Controller class
@@ -22,12 +25,46 @@ public class MainController implements Initializable {
      * Initializes the controller class.
      */
     
+  @FXML
+  private CheckBox cbDog;
+  @FXML
+  private CheckBox cbCat;
+  @FXML
+  private CheckBox cbBird;
+  @FXML
+  private CheckBox cbCow;
+  @FXML
+  private Label lblList;
+  @FXML
+  private Label lblCount;
   
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
     
+    public void checkEvent(ActionEvent event){
+        int count = 0;
+        String message = "";
+        if(cbDog.isSelected()){
+            count++;
+            message += cbDog.getText() + "\n";
+        }
+        if(cbCat.isSelected()){
+            count++;
+            message += cbCat.getText() + "\n";
+        }
+        if(cbBird.isSelected()){
+            count++;
+            message += cbBird.getText() + "\n";
+        }
+        if(cbCow.isSelected()){
+            count++;
+            message += cbCow.getText() + "\n";
+        }
+        lblCount.setText("Items Selected: " + count);
+        lblList.setText(message);
+    }
       public void closeApp(ActionEvent event){
           Platform.exit();
           System.exit(0);
