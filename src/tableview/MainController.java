a0bda77ee762947837bfed0504e22c6639bf14b3
NemanjaMandic <nemanjans84@gmail.com>
2016-02-04 05:44:54 +0100

    table view

--- src/tableview/MainController.java
+++ src/tableview/MainController.java
@@ -7,7 +7,13 @@ package tableview;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 /**
  * FXML Controller class
@@ -16,12 +22,37 @@ import javafx.fxml.Initializable;
  */
 public class MainController implements Initializable {
 
+    @FXML private TableView<Student> studentsTable;
+   
+    @FXML private TableColumn<Student, Integer> idColumn;
+    @FXML private TableColumn<Student, String> nameColumn;
+    @FXML private TableColumn<Student, String> surnameColumn;
+    @FXML private TableColumn<Student, Integer> ageColumn;
+    
+    public ObservableList<Student> list = FXCollections.observableArrayList(
+      new Student(1, "Mark", "Johnson", 19),
+      new Student(2, "John", "Johnson", 39),
+      new Student(3, "Derek", "Johnson", 22),
+      new Student(4, "Robert", "Johnson", 43),
+      new Student(5, "Mike", "Johnson", 49),
+      new Student(6, "Duke", "Johnson", 55),
+      new Student(7, "Joan", "Johnson", 50),
+      new Student(8, "Jennifer", "Johnson", 44),
+      new Student(9, "Tom", "Johnson", 15),
+      new Student(10, "Brad", "Johnson", 66)
+    );
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        idColumn.setCellValueFactory(new PropertyValueFactory<>("ID"));
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("Name"));
+        surnameColumn.setCellValueFactory(new PropertyValueFactory<>("Surname"));
+        ageColumn.setCellValueFactory(new PropertyValueFactory<>("Age"));
+       
+        studentsTable.setItems(list);
     }    
     
 }
--- src/tableview/Student.java
+++ src/tableview/Student.java
@@ -1,14 +1,48 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package tableview;
 
-/**
- *
- * @author nemus
- */
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+
 public class Student {
+    private final SimpleIntegerProperty id;
+    private final SimpleStringProperty name;
+    private final SimpleStringProperty surname;
+    private final SimpleIntegerProperty age;
     
+    public Student(Integer id, String name, String surname, Integer age){
+        this.id = new SimpleIntegerProperty(id);
+        this.name = new SimpleStringProperty(name);
+        this.surname = new SimpleStringProperty(surname);
+        this.age = new SimpleIntegerProperty(age);
+    }
+
+    /**
+     * @return the id
+     */
+    public Integer getId() {
+        return id.get();
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name.get();
+    }
+
+    /**
+     * @return the surname
+     */
+    public String getSurname() {
+        return surname.get();
+    }
+
+    /**
+     * @return the age
+     */
+    public Integer getAge() {
+        return age.get();
+    }
 }
